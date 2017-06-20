bfe11db2f0716b73a3f1959a0a8fe9cff2575710
m.malkin <mmalkin@fun-box.ru>
2017-06-20 13:16:57 +0300

    binstubs simplified

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Exreleasy.Mixfile do
   def project do
     [
       app: :exreleasy,
-      version: "0.1.7",
+      version: "0.1.8",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
